1c6c649612513dfb0896a7cccf7f705412391476
emanuelabreu13 <emanuel_abreu.2@outlook.com>
2023-04-19 20:57:50 -0300

    retirada do .env

--- src/database/labmedicinebd.js
+++ src/database/labmedicinebd.js
@@ -2,12 +2,12 @@ const { Sequelize } = require("sequelize");
 
 // CRIANDO A CONEXÃO COM O BANCO DE DADOS
 const connection = new Sequelize({
-  dialect: process.env.DIALECT_DATABASE,
-  host: process.env.HOST_DATABASE,
-  username: process.env.USER_DATABASE,
-  password: process.env.PASSWORD_DATABASE,
-  port: process.env.PORT_DATABASE,
-  database: process.env.NAME_DATABASE,
+  dialect: "postgres", // qual banco vai se conecta
+  host: "localhost", //onde o banco está ?
+  username: "emanuel", //qual usuario
+  password: "emanuel", // qual senha
+  port: "5432", // qual a porta
+  database: "labmedicinebd", //qual nome de dados
   define: {
     underscored: true,
     underscoredAll: true,
